a592dcd27d5810cf96339d30335ec1ecb7d70f79
Xifip <costelljohn@gmail.com>
2014-10-30 12:53:33 +0000

    validations on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,9 @@ class Product < ActiveRecord::Base
   has_many :spins, dependent: :destroy
 
   validates_uniqueness_of :name, :product_number
-
+  validates_presence_of :name
+  
+  validates :product_number, :numericality => { :less_than_or_equal_to => 100 }, :presence => true
   def latest_spin_name
     name_last_spin = self.spins.order("created_at").last.name
     name_last_spin 
